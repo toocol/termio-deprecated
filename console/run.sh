f0c7fadc775981ed7fe75c983510c147234abf01
Joe Zane <joezane.cn@gmail.com>
2022-08-30 20:52:16 +0800

    1. update script file: add vm options parameter

--- console/run.sh
+++ console/run.sh
@@ -2,4 +2,5 @@
 $JAVA_HOME/bin/java \
 -jar \
 -Xdebug -Xrunjdwp:transport=dt_socket,suspend=n,server=y,address=5500 \
+-XX:G1PeriodicGCInterval=1000 -XX:G1PeriodicGCSystemLoadThreshold=0 \
 target/termio.jar
