3fb3eaff482fe4a63de2f555ace30a098a000963
JoeZane <joezane.cn@gmail.com>
2022-09-05 01:21:46 +0800

    1. update terminalemulator in nativefx

--- nativefx/src/cpp/terminalemulator.cpp
+++ nativefx/src/cpp/terminalemulator.cpp
@@ -1,5 +1,21 @@
 #include "terminalemulator.h"
 
-TerminalEmulator::TerminalEmulator(QWidget *parent) : QWidget(parent) {}
+TerminalEmulator::TerminalEmulator(QWidget *parent) : QWidget(parent) {
+  resize(1280, 800);
+  textDisplay = new QTextEdit(this);
+  textDisplay->setReadOnly(true);
+  textDisplay->resize(1280, 800);
+  textDisplay->setTextColor(QColor(0xba, 0xb1, 0x2d));
+  textDisplay->setText(
+      "kkkkkkkkkk\n"
+      "kkkkkkkkkk\n"
+      "kkkkkkkkkk\n");
+  textDisplay->setTextColor(QColor(0xbb, 0x11, 0xaa));
+  QTextDocument *document = textDisplay->document();
+  QTextBlock block = document->findBlockByLineNumber(2);
+  QTextCursor cursor = textDisplay->textCursor();
+  cursor.setPosition(block.position());
+  cursor.insertText("HelloWorld~");
+}
 
-TerminalEmulator::~TerminalEmulator() {}
+TerminalEmulator::~TerminalEmulator() { delete textDisplay; }
--- nativefx/src/headers/terminalemulator.h
+++ nativefx/src/headers/terminalemulator.h
@@ -1,10 +1,14 @@
 #ifndef TERMINALEMULATOR_H
 #define TERMINALEMULATOR_H
 
+#include <QTextBlock>
+#include <QTextEdit>
 #include <QWidget>
 
 class TerminalEmulator : public QWidget {
   Q_OBJECT
+ private:
+  QTextEdit *textDisplay;
 
  public:
   TerminalEmulator(QWidget *parent = nullptr);
